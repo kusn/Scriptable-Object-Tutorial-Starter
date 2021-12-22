8b46935964f2598dcfb6c8b84d9b660e1f53b563
Kusn <serjikf@yandex.ru>
2021-12-22 15:48:20 +0300

    Продолжение

--- Assets/RW/Scripts/Sword.cs
+++ Assets/RW/Scripts/Sword.cs
@@ -32,14 +32,10 @@ using UnityEngine;
 public class Sword : MonoBehaviour
 {
     [SerializeField]
-    private SwordData swordData; // 1
+    private GameEvent OnSwordSelected; // 1
 
-    private void OnMouseDown()  // 2
+    private void OnMouseDown()
     {
-        Debug.Log(swordData.SwordName);  // 3
-        Debug.Log(swordData.Description);  // 3
-        Debug.Log(swordData.Icon.name);  // 3
-        Debug.Log(swordData.GoldCost);  // 3
-        Debug.Log(swordData.AttackDamage);  // 3
+        OnSwordSelected.Raise(); // 2
     }
 }
